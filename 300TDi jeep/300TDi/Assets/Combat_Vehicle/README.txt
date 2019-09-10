682b4cd1d61f10882c4ddfc19dba8c489e23da6f
vidhantmatta <mattavidhant@gmail.com>
2019-09-10 23:51:53 +0530

    added 300tdi

---
+++ 300TDi jeep/300TDi/Assets/Combat_Vehicle/README.txt
@@ -0,0 +1,17 @@
+Created & imported into unity by Yoon's GameART
+
+free to use this model for any type of project
+
+Free 3D prop
+
+Military Land  "Combat Vehicle" Ver 1.02
+
+using Hand made Texture map include
+
+PBR Shader Change
+
+diffuse map 1024x1024
+norma l map 512x512 
+
+vertex  : 1648
+polygon : 2533
---
+++ 300TDi jeep/300TDi/Assets/getengrpm.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LitJson;
+using System.IO;
+using System;
+using SimpleJSON;
+
+
+
+public class getengrpm : MonoBehaviour
+{
+
+    public Text rpm;
+    public Text temp;
+    public Text pressure;
+    public Text mileage;
+    bool flag = true;
+    // Coroutine getCor = null;
+    public string title;
+
+    string url = "http://theminimalist.esy.es/getrpmAR.php?name=fan";
+    private string a;
+
+    void Awake()
+    {
+
+
+        StartCoroutine(getText());
+
+    }
+
+
+
+
+    IEnumerator getText()
+    {
+        while (flag == true)
+        {
+            WWW www = new WWW(url);
+            yield return www;
+            //readText.text = www.text;
+            string json1= www.text;
+             JSONNode jsonnode = SimpleJSON.JSON.Parse(json1); //most imp
+            //Debug.Log(jsonnode["r"][0]["temp"].ToString());
+            rpm.text = jsonnode["r"][0]["rpm"];
+            temp.text= jsonnode["r"][0]["temp"];
+            pressure.text= jsonnode["r"][0]["pressure"];
+            mileage.text= jsonnode["r"][0]["mileage"];
+
+            
+
+
+        }
+        yield return new WaitForSeconds(2);
+
+    }
+   
+}
